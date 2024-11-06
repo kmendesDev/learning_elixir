86f825bc40cb3b102a7f23c74259b3d71e6e2a6e
Kaio Pablo Carvalho Mendes <kaiopablo17@gmail.com>
2024-11-05 22:54:07 -0300

    program with Exunit and mock

--- test/elixir_teste_test.exs
+++ test/elixir_teste_test.exs
@@ -1,8 +1,13 @@
 defmodule ElixirTesteTest do
   use ExUnit.Case
-  doctest ElixirTeste
+  doctest ElixirTeste # Testes que usam o exemplo da documentação
 
-  test "greets the world" do
+  test "greets the world" do # Descrição
     assert ElixirTeste.hello() == :world
   end
+
+ # @tag falha: true
+ # test "falha de propósito" do
+ #   assert ElixirTeste.hello() != :world
+ # end
 end
